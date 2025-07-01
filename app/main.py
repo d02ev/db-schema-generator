a260c6301b203418daf0d913554c0950661fc886
d02ev <vikramadityapratapsingh98@gmail.com>
2025-07-02 02:28:25 +0530

    add global exception handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.api.v1 import health
+from app.core.exceptions import GlobalExceptionMiddleware
 
 app = FastAPI(title="DB Schema Generator API", version="1.0.0")
+
+app.add_middleware(GlobalExceptionMiddleware)
 
 app.include_router(health.router)
